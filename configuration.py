7ad6ddadd5118e2096d0540dd4d97960f6d9fa12
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2020-12-31 17:50:11 +0200

    final changes, we are now prepared for Haganot

---
+++ configuration.py
@@ -0,0 +1,8 @@
+import socket
+local_ip_addr = socket.gethostbyname(socket.gethostname())
+udp_port = 12000
+broadcast_addr = "255.255.255.255"
+broadcast_port = 13117
+wait_time = 10
+tcp_port = 2112
+team_name = 'RedHatYossi\n'
